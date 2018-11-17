a2dee4532f33957cdbd721018ff340c3eb6ffecd
oleksii <a_vodka@mail.ru>
2018-11-18 00:14:10 +0200

    upds

--- MircoCellularAutomaton.py
+++ MircoCellularAutomaton.py
@@ -4,13 +4,14 @@ import random
 
 class MircoCellularAutomaton:
 
-    def __init__(self, width, height, neighbour='moore', neighbour_matrix=None):
+    def __init__(self, width, height, neighbour='moore', neighbour_matrix=None, periodic=False):
         self.newdata = None
         self.data = np.zeros((width, height), dtype=int)
         self.w = width
         self.h = height
         self.neighbour_method = self.moore
         self.num_of_cells = 0
+        self.periodic = periodic
         if neighbour == 'moore':
             self.neighbour_method = self.moore
         if neighbour == 'von_neumann':
@@ -62,8 +63,13 @@ class MircoCellularAutomaton:
     def next_iteration(self):
         self.newdata = np.copy(self.data)
 
-        for i in range(-2, self.w - 1):
-            for j in range(-2, self.h - 1):
+        if self.periodic:
+            offset = -2
+        else:
+            offset = 0
+
+        for i in range(offset, self.w - 1):
+            for j in range(offset, self.h - 1):
                 self.neighbour_method(i, j)
         self.data = self.newdata
         pass
@@ -72,6 +78,19 @@ class MircoCellularAutomaton:
         self.num_of_cells = num
         for i in range(num + 1):
             self.data[random.randint(0, self.w - 1), random.randint(0, self.h - 1)] = i
+        pass
+
+    def initial_cell_mesh(self, rows=5, cols=5):
+        self.num_of_cells = rows * cols
+        col_step = self.w / cols
+        row_step = self.h / rows
+        k = 1
+        for i in range(rows):
+            for j in range(cols):
+                ii = int((i + 0.5) * row_step)
+                jj = int((j + 0.5) * col_step)
+                self.data[ii, jj] = k
+                k += 1
         pass
 
     def add_new_centers(self, num):
@@ -96,14 +115,26 @@ class MircoCellularAutomaton:
             zero_cell = self.w * self.h - np.count_nonzero(self.data)
 
             if verbose:
-                print i, zero_cell
+                print i, zero_cell, "{0}%".format(zero_cell * 100.0 / self.w / self.h)
             i += 1
 
-            #self.add_new_centers(10)
+            # self.add_new_centers(1)
 
             if zero_cell == 0:
                 break
         pass
+
+    def toBlackAndWhite(self):
+        bw_image = np.zeros_like(self.data)
+        for i in range(-2, self.w - 1):
+            for j in range(-2, self.h - 1):
+                cond = self.data[i, j] == self.data[i, j + 1] and self.data[i, j] == self.data[i, j - 1] and self.data[
+                    i + 1, j] and self.data[i, j] == self.data[i - 1, j]
+                if cond:
+                    bw_image[i, j] = 1.
+                else:
+                    bw_image[i, j] = 0.
+        return bw_image
 
     def print_data(self):
         print self.data
--- main.py
+++ main.py
@@ -2,24 +2,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal
 import random
+import skimage.measure
 from MircoCellularAutomaton import *
 
 random.seed(0)
 prob_matrix = np.array([[ 0.1, 0.1, 0.1], [0.1, 1, 0.1], [ 0.1, 0.1,  0.1]], dtype=float)
 
-ca = MircoCellularAutomaton(200, 200, neighbour='moore', neighbour_matrix=prob_matrix)
-ca.initial_cells(20)
+ca = MircoCellularAutomaton(100, 100, neighbour='moore', neighbour_matrix=prob_matrix, periodic=False)
+ca.initial_cells(100)
+#ca.initial_cell_mesh()
 ca.calculate(verbose=True)
-
-sobel = scipy.ndimage.prewitt(ca.data)
-# sobel[sobel != 0] = 1
 
 plt.subplot(1, 2, 1)
 plt.imshow(ca.data, interpolation='none')
 plt.subplot(1, 2, 2)
 
-plt.imshow(sobel, interpolation='none', cmap='gray')
+bwimage = ca.toBlackAndWhite()
+
+label_img = skimage.measure.label(ca.data)
+regions = skimage.measure.regionprops(label_img)
+
+fig, ax = plt.subplots()
+ax.imshow(ca.data, cmap=plt.cm.gray)
+
+for props in regions:
+    y0, x0 = props.centroid
+    orientation = props.orientation
+    x1 = x0 + np.cos(orientation) * 0.5 * props.major_axis_length
+    y1 = y0 - np.sin(orientation) * 0.5 * props.major_axis_length
+    x2 = x0 - np.sin(orientation) * 0.5 * props.minor_axis_length
+    y2 = y0 - np.cos(orientation) * 0.5 * props.minor_axis_length
+
+    ax.plot((x0, x1), (y0, y1), '-r', linewidth=0.5)
+    ax.plot((x0, x2), (y0, y2), '-r', linewidth=0.5)
+    ax.plot(x0, y0, '.g', markersize=3)
+
+    minr, minc, maxr, maxc = props.bbox
+    bx = (minc, maxc, maxc, minc, minc)
+    by = (minr, minr, maxr, maxr, minr)
+    ax.plot(bx, by, '-b', linewidth=2.5)
+
+#ax.axis((0, 600, 600, 0))
+plt.show()
+
+print label_img, regions
+
+plt.imshow(bwimage, interpolation='none', cmap='gray')
 plt.tight_layout()
 plt.show()
 
-np.savetxt('img20.txt',ca.data, delimiter=';')
+#np.savetxt('img20.txt',ca.data, delimiter=';')
